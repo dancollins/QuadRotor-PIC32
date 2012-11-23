ed480d3175c7d16ae37f7c9d7336b41c4aeb6348
Dan <microman171@gmail.com>
2012-11-24 00:06:51 +1300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,23 @@
+/**
+ * @author: Dan
+ * @since November 23, 2012, 11:57 PM
+ * @version 1.0
+ *
+ * DESC
+ */
+
+#include <xc.h>
+
+void init(void) {
+
+}
+
+int main(void) {
+    init();
+
+    while (1) {
+
+    }
+
+    return 0;
+}
